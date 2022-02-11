c7da3428b471dbdbea8f922ceada43ea5e3c9231
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-02-11 12:00:29 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 const employee = {}
 
 function updateEmployeeWithKeyAndValue(employee,key,value) {
-    updatedEmployee = {...employee,
+    const updatedEmployee = {...employee,
             [key]: value}
     return updatedEmployee
+    }
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+     employee[key]= value   
+     return employee
+    }
+
+    function deleteFromEmployeeByKey(employee, key) {
+        const newEmployee = {...employee}
+        delete newEmployee[key]
+        return newEmployee
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key]
+        return employee
     }
