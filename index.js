16b2590e2467ff5c842edcc1196467e0f748d811
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-02-11 11:37:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+const employee = {}
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    updatedEmployee = {...employee,
+            [key]: value}
+    return updatedEmployee
+    }
